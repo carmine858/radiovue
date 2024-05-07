4a8a15f51e380f0f335ba81f90be96b3a9a4b4ea
carmine858 <72042419+carmine858@users.noreply.github.com>
2024-05-07 09:59:54 +0000

    init

---
+++ radio/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav>
+  <v-layout class="overflow-visible" style="height: 56px;">
+    <v-bottom-navigation
+      v-model="value"
+      :bg-color="color"
+      mode="shift"
+    >
+     
+
+      <v-btn>
+        <v-icon>mdi-music-note</v-icon>
+
+        <span>Music</span>
+      </v-btn>
+       <v-btn>
+        <v-icon>mdi-television-play</v-icon>
+
+        <span>Video</span>
+      </v-btn>
+
+    </v-bottom-navigation>
+  </v-layout>
+
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+export default {
+    data: () => ({ value: 0 }),
+
+    computed: {
+      color () {
+        switch (this.value) {
+          case 0: return 'teal'
+          case 1: return 'brown'
+          case 2: return 'blue-grey'
+          case 3: return 'blue-grey'
+          default: return 'blue-grey'
+        }
+      },
+    },
+    name: 'HomeView',
+  }
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ radio/src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <h1>Online Radio</h1>
+  </v-container>
+
+  <template>
+  <v-row>
+    <v-col
+      v-for="n in 9"
+      :key="n"
+      class="d-flex child-flex"
+      cols="4"
+    >
+      
+        <template v-slot:placeholder>
+          <v-row
+            align="center"
+            class="fill-height ma-0"
+            justify="center"
+          >
+            <v-progress-circular
+              color="grey-lighten-5"
+              indeterminate
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      
+    </v-col>
+  </v-row>
+</template>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      radios: [],
+
+    }
+  },
+  methods: {
+    getRadios() {
+      fetch('https://nl1.api.radio-browser.info/json/stations/search?limit=100&countrycode=IT&hidebroken=true&order=clickcount&reverse=true')
+        .then(response => response.json())
+        .then(data => {
+          this.radios = data;
+          console.log(data);
+        });
+
+    },
+  },
+  created() {
+    this.getRadios();
+  },
+}
+</script>
