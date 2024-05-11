f307cf5d15fabdffb5afbabf09ce4dc434e0417f
carmine858 <72042419+carmine858@users.noreply.github.com>
2024-05-11 09:47:18 +0000

    cerca funzionante

--- radio/src/views/HomeView.vue
+++ radio/src/views/HomeView.vue
@@ -1,52 +1,11 @@
 <template>
   <v-container>
     <div class="text-h1 titolo">ONLINE RADIO</div><br>
-    <v-combobox
-      v-model="selectedRadio"
-      :items="radioNames()"
-      label="Cerca Radio"
-      @input="filterRadios"
-      @keydown.enter="handleEnter()"
-      variant="underlined"
-    >
-      <template #item="{ item }">
-        <v-list-item @click="selectRadio(item)">
-          <v-list-item-content>{{ item.title }}</v-list-item-content>
-        </v-list-item>
-      </template>
-      <template #selection="{ item }">
-        <v-list-item @click="selectRadio(item)">
-          <v-list-item-content>{{ item.title }}</v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-combobox>
+    <v-text-field v-model="searchQuery" label="Cerca per nome" outlined></v-text-field>
 
-    <v-container v-if="Object.keys(filteredRadios).length > 0">
+    <v-container>
       <v-row>
-        <v-col v-for="(radio, index) in filteredRadios" :key="index" cols="12" sm="6" md="4" lg="3">
-          <v-card class="mx-auto" max-width="350" elevation="9" color="#ffffff">
-            <v-avatar class="ma-3" rounded="0" size="125">
-              <v-img :src="getRadioImage(radio)" height="125" width="auto"></v-img>
-            </v-avatar>
-            <div class="d-flex flex-column justify-space-between">
-              <div>
-                <v-card-title class="text-h5">{{ radio.name }}</v-card-title>
-                <v-card-subtitle>{{ radio.Country }}</v-card-subtitle>
-              </div>
-              <v-card-actions>
-                <v-btn :icon="radio.isPlaying ? 'mdi-stop' : 'mdi-play'" @click="togglePlayback(radio)"></v-btn>
-                <v-btn :icon="radio.isFavorite ? 'mdi-heart' : 'mdi-heart-outline'" @click="toggleFavorite(radio)"></v-btn>
-              </v-card-actions>
-            </div>
-          </v-card>
-          <audio ref="audioPlayer"></audio>
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <v-container v-else>
-      <v-row>
-        <v-col v-for="(radio, index) in radios" :key="index" cols="12" sm="6" md="4" lg="3">
+        <v-col v-for="(radio, index) in filterRadios" :key="index" cols="12" sm="6" md="4" lg="3">
           <v-card class="mx-auto" max-width="350" elevation="9" color="#ffffff">
             <v-avatar class="ma-3" rounded="0" size="125">
               <v-img :src="getRadioImage(radio)" height="125" width="auto"></v-img>
@@ -77,41 +36,24 @@ export default {
   data() {
     return {
       radios: [],
-      filteredRadios: {},
+     
       selectedRadio: null,
+      searchQuery: '',
 
       audio: new Audio(),
       currentPlayingRadio: null,
       hls: null
     };
   },
-  methods: {
-    handleEnter() {
-      for (const key in this.filteredRadios) {
-    if (this.filteredRadios[key].name.toLowerCase() === this.selectedRadio.toLowerCase()) {
-      this.selectRadio(this.filteredRadios[key]);
-      return; // Termina il ciclo dopo aver trovato la radio corrispondente
-    }
-  }
-    },
-    selectRadio(radio) {
-      if (!this.filteredRadios[radio.name]) {
-        this.filteredRadios = { ...this.filteredRadios, [radio.name]: radio };
-      }
-    },
+  computed: {
+    // Filtraggio delle radio in base alla query di ricerca
     filterRadios() {
-      if (this.selectedRadio) {
-        this.filteredRadios = this.radios.filter(radio => radio.name.toLowerCase().includes(this.selectedRadio.toLowerCase()));
-      } else {
-        this.filteredRadios = {};
-      }
-    },
-    radioNames() {
-      return this.radios.map(radio => ({
-        title: radio.name,
-        value: radio.name
-      }));
-    },
+      return this.radios.filter(radio => {
+        return radio.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+      });
+    }
+  },
+  methods: {
     getRadios() {
       const storedRadios = localStorage.getItem('radios');
       if (storedRadios != this.radios.Country === "Italy") {
